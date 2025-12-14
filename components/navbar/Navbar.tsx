16826fd239645626b7d80a05a2074120f49fff65
Mario Robayo <mario.robayo@ninjarmm.com>
2025-12-14 00:11:25 -0500

    add shadcn

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+
+
+function Navbar() {
+  return (
+    <nav className='border-b'>
+      <div className='container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8'>
+
+        React challenges
+
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
